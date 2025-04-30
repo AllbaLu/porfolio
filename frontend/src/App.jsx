dca7ccc099a5ab9c0ac00824170bfd6099805ea2
albal <albaluccia@gmail.com>
2025-04-30 16:09:37 +0800

    added background to index.css-gradiante

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,19 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+
 import './App.css'
 import { Navbar } from './components/Navbar'
 import { Hero } from './sections/Hero'
-import { Projects } from './sections/Projects'
-import { Contact } from './sections/Contact'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className='bg-pink'>gola</div>
+      <div className=''></div>
       <Navbar />
       <Hero />
-      <Projects />
-      <Contact />
     </>
   )
 }
